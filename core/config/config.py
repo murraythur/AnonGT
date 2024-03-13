ff5989d1a84277ca03ef1c9344b8263719200855
Arthur M <143713077+murraythur@users.noreply.github.com>
2024-03-12 22:26:22 -0300

    config.py

---
+++ core/config/config.py
@@ -0,0 +1,21 @@
+# Anongt Vver
+VERSION = "0.1.1"
+
+# exclude
+TOR_EXCLUDE = "192.168.0.0/16 172.16.0.0/12 10.0.0.0/8"
+TOR_UID = "debian-tor"
+
+# port
+TOR_PORT = "9040"
+
+# tor dns port
+TOR_DNS = "9053"
+
+# tor config files
+TORRC = "/etc/tor/torrc"
+
+# backup dir
+BACKUPDIR = "/var/lib/anongt"
+
+# current dir
+CURRTENTDIR = "/usr/share/AnonGT/"
